0f0be65d93f63c6e95a933d6c6077a5e0cb69442
User44314 <user1234aaaa@gmail.com>
2023-06-13 08:35:01 +0300

    feat: fix some code and add scripts

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,9 @@ module.exports = {
     ],
   },
   output: {
+    // publicPath: "public",
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
   },
+  mode: "development"
 };
